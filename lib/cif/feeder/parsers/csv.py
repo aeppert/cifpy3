7b648d09ee83ede7c862d9367ae71c1adce74926
jmdevince <james@hexhost.net>
2015-10-08 14:50:41 -0500

    Update csv.py

--- lib/cif/feeder/parsers/csv.py
+++ lib/cif/feeder/parsers/csv.py
@@ -22,7 +22,7 @@ class Csv(Parser):
 
         observables = []
 
-        if self.total_objects == 0 and "start" in self.parsing_details and self.parsing_details["start"] > 0:
+        if self.total_objects == 0 and "start" in self.parsing_details and self.parsing_details["start"] > 1:
             for x in range(1, self.parsing_details["start"]+1):
                 try:
                     next(self.csv)

683016aeeafe5c6da483dc4ce1459f0da6111039
jmdevince <james@hexhost.net>
2015-10-08 14:36:56 -0500

    Update regex.py

--- lib/cif/feeder/parsers/regex.py
+++ lib/cif/feeder/parsers/regex.py
@@ -69,8 +69,8 @@ class Regex(Parser):
             observable = self.create_observable_from_meta_if_not_in_journal(results)
             if observable is not None:
                 observables.append(observable)
-                self.total_objects += 1
                 objects += 1
+            self.total_objects += 1
 
             if self.ending and self.total_objects >= self.end:
                 self.parsing = False

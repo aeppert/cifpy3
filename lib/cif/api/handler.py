8a086c2516c418a81bc84e19662467dce4034a7e
jmdevince <james@hexhost.net>
2015-10-09 13:34:57 -0500

    Update handler.py

--- lib/cif/api/handler.py
+++ lib/cif/api/handler.py
@@ -17,13 +17,13 @@ class Handler(http.server.BaseHTTPRequestHandler):
 
     def connect_to_backend(self):
         # Based on the startup options for cif-server, let's get the backend + instantiate the class from that module
-        self.logging.debug("Loading backend: {0}".format(cif.options.storage.lower()))
+        self.server.logging.debug("Loading backend: {0}".format(cif.options.storage.lower()))
         self.backend = getattr(__import__("cif.backends.{0}".format(
             cif.options.storage.lower()), fromlist=[cif.options.storage.title()]), cif.options.storage.title()
         )()
 
         # Connect to the backend
-        self.logging.debug("Connecting to backend: {0}".format(cif.options.storage_uri))
+        self.server.logging.debug("Connecting to backend: {0}".format(cif.options.storage_uri))
         self.backend.connect(cif.options.storage_uri)
 
     def check_authentication(self):

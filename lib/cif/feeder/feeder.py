0057983a60ee33db2150a9a613e196f6e1ef9c93
jmdevince <james@hexhost.net>
2015-10-12 14:27:56 -0500

    Update feeder.py

--- lib/cif/feeder/feeder.py
+++ lib/cif/feeder/feeder.py
@@ -4,6 +4,8 @@ import os
 import multiprocessing
 import time
 
+import schedule
+
 import cif
 
 
@@ -12,40 +14,40 @@ class Feeder(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
         self.backend = None
         self.logging = cif.logging.getLogger('FEEDER')
+        self.logging.info("Loading Feeds")
+        self.load_feeds()
 
+    def load_feeds(self):
+        schedule.clear()
+        feeds = {}
+        self.logging.debug("Getting List of Feeds")
+        files = os.listdir(cif.options.feed_directory)
+        feed_files = []
+        for file in files:
+            if file.endswith(".yml"):
+                self.logging.debug("Found Feed File: {0}".format(file))
+                feed_files.append(os.path.join(cif.options.feed_directory, file))
+
+        feed_files.sort()
+        for feed_file in feed_files:
+            self.logging.info("Loading Feed File: {0}".format(feed_file))
+            feeds[feed_file] = cif.feeder.Feed(feed_file).
+            self.logging.info("Scheduling Feed File:".format(feed_file))
+            for feed_name in feeds[feed_file]['feed_config']['feeds'].keys():
+                if "interval" in feeds[feed_file]['feed_config']['feeds'][feed_name]:
+                    if feeds[feed_file]['feed_config']['feeds'][feed_name]['interval'] == "hourly":
+                        schedule.every().hour.do(feeds[feed_file].process, feed_name)
+                    elif feeds[feed_file]['feed_config']['feeds'][feed_name]['interval'] == "daily":
+                        schedule.every().day.at("00:00").do(feeds[feed_file].process, feed_name)
+                    elif feeds[feed_file]['feed_config']['feeds'][feed_name]['interval'] == "weekly":
+                        schedule.every().day.at("00:00").do(feeds[feed_file].process, feed_name)
+                    else:
+                        schedule.every().hour.do(feeds[feed_file].process, feed_name)
+                else:
+                    schedule.every().hour.do(feeds[feed_file].process, feed_name)
+                
+                
     def run(self):
         while True:
-            self.logging.info("Starting feeder Run")
-            # Based on the startup options for cif-server, get the backend + instantiate the class from that module
-            self.logging.debug("Loading backend: {0}".format(cif.options.storage.lower()))
-            backend = getattr(__import__("cif.backends.{0:s}".format(
-                cif.options.storage.lower()), fromlist=[cif.options.storage.title()]), cif.options.storage.title()
-            )()
-
-            self.logging.debug("Connecting to backend: {0}".format(cif.options.storage_uri))
-            backend.connect(cif.options.storage_uri)
-
-            self.logging.debug("Getting List of Feeds")
-            files = os.listdir(cif.options.feed_directory)
-            feed_files = []
-            for file in files:
-                if file.endswith(".yml"):
-                    self.logging.debug("Found Feed File: {0}".format(file))
-                    feed_files.append(os.path.join(cif.options.feed_directory, file))
-
-            feed_files.sort()
-            for feed_file in feed_files:
-                self.logging.info("Loading Feed File: {0}".format(feed_file))
-                feed = cif.feeder.Feed(feed_file)
-
-                self.logging.info("Running Feed File:".format(feed_file))
-                try:
-                    feed.process()
-                except Exception as e:
-                    self.logging.exception('Exception while Running Feed {0}: {1}'.format(feed_file, e))
-
-            self.logging.debug("Disconnecting from backend")
-            backend.disconnect()
-
-            self.logging.info("Sleeping feeder for {0} seconds".format(cif.options.feed_interval))
-            time.sleep(cif.options.feed_interval)
+            schedule.run_pending()
+            time.sleep(1)

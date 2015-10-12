29f9a8cac73da50e443e27282bc96e478a9095b6
jmdevince <james@hexhost.net>
2015-10-12 14:16:53 -0500

    Update feed.py

--- lib/cif/feeder/feed.py
+++ lib/cif/feeder/feed.py
@@ -47,119 +47,126 @@ class Feed(object):
 
         self.feed_config = feed_config
 
-    def process(self):
+    def processall(self);
+        if "feeds" not in self.feed_config.keys():
+            return
+        for feed_name, feed in self.feed_config["feeds"].items():
+            self.process(self, feed_name)
+            
+    def process(self, feed_name):
         """Retrieves the feed, passes it to a parser, and then passes parsed observables to the workers
 
         :return:
         """
         if "feeds" not in self.feed_config.keys():
             return
+        if name not in self.feed_config['feeds']:
+            return
+        
+        # These are fields that are used for control when parsing and should not be passed down to the observables
+        fields_to_strip = ['node', 'map', 'values', 'pattern', 'remote', 'parser',
+                           'username', 'password', 'method', 'start', 'end', 'interval']
 
-        for feed_name, feed in self.feed_config["feeds"].items():
-            # These are fields that are used for control when parsing and should not be passed down to the observables
-            fields_to_strip = ['node', 'map', 'values', 'pattern', 'remote', 'parser',
-                               'username', 'password', 'method', 'start', 'end', 'interval']
+        # Pull out parsing details for feeds from defined meta
+        feed_parsing_details = dict((name, self.feed_config["feeds"][name]) for name in fields_to_strip if name in self.feed_config["feeds"].keys())
+        feed_parsing_details['feed_name'] = feed_name
 
-            # Pull out parsing details for feeds from defined meta
-            feed_parsing_details = dict((name, feed[name]) for name in fields_to_strip if name in feed.keys())
-            feed_parsing_details['feed_name'] = feed_name
+        # Exclude control fields from defined meta for created observables
+        feed_meta = dict((name, self.feed_config["feeds"][name]) for name in self.feed_config["feeds"].keys() if name not in fields_to_strip)
 
-            # Exclude control fields from defined meta for created observables
-            feed_meta = dict((name, feed[name]) for name in feed.keys() if name not in fields_to_strip)
+        if "method" not in feed_parsing_details.keys():
+            feed_parsing_details["method"] = "GET"
 
-            if "method" not in feed_parsing_details.keys():
-                feed_parsing_details["method"] = "GET"
+        if "parser" not in feed_parsing_details.keys():
+            feed_parsing_details["parser"] = "regex"
 
-            if "parser" not in feed_parsing_details.keys():
-                feed_parsing_details["parser"] = "regex"
+        if "values" not in feed_parsing_details.keys():
+            feed_parsing_details["values"] = ["observable"]
+        elif not isinstance(feed_parsing_details["values"], list):
+            feed_parsing_details["values"] = [feed_parsing_details["values"]]
 
-            if "values" not in feed_parsing_details.keys():
-                feed_parsing_details["values"] = ["observable"]
-            elif not isinstance(feed_parsing_details["values"], list):
-                feed_parsing_details["values"] = [feed_parsing_details["values"]]
+        feed_parsing_details["journal"] = "{0}/{1}-{2}-{3}-journal.pickle".format(
+            cif.CACHEDIR, os.path.basename(self.feed_file.lower()), feed_name.lower(),
+            datetime.datetime.utcnow().strftime("%Y-%m-%d")
+        )
+        self.logging.debug("Built Journal Path: {0}".format(feed_parsing_details['journal']))
 
-            feed_parsing_details["journal"] = "{0}/{1}-{2}-{3}-journal.pickle".format(
-                cif.CACHEDIR, os.path.basename(self.feed_file.lower()), feed_name.lower(),
-                datetime.datetime.utcnow().strftime("%Y-%m-%d")
+        if feed_parsing_details["remote"].startswith('/'):
+            self.logging.debug("Local file detected '{0}'. Opening Locally.".format(feed_parsing_details["remote"]))
+            temp = open(feed_parsing_details["remote"], "rb")
+        else:
+            self.logging.debug("Remote file detected '{0}'. Opening remotely.".format(
+                feed_parsing_details["remote"])
             )
-            self.logging.debug("Built Journal Path: {0}".format(feed_parsing_details['journal']))
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/45.0.2454.93 Safari/537.36'
+            }
+            try:
+                response = requests.request(feed_parsing_details["method"], feed_parsing_details["remote"],
+                                            proxies=cif.proxies, stream=True, headers=headers, verify=False
+                                            )
+            except:
+                self.logging.exception("Could not fetch remote url '{0}'".format(feed_parsing_details["remote"]))
+                continue
 
-            if feed_parsing_details["remote"].startswith('/'):
-                self.logging.debug("Local file detected '{0}'. Opening Locally.".format(feed_parsing_details["remote"]))
-                temp = open(feed_parsing_details["remote"], "rb")
-            else:
-                self.logging.debug("Remote file detected '{0}'. Opening remotely.".format(
-                    feed_parsing_details["remote"])
+            if response.status_code > 300:
+                self.logging.error("Failed fetching remote feed '{0}': {1} {2}".format(
+                    feed_parsing_details["remote"], response.status_code, response.reason)
                 )
-                headers = {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                    'Chrome/45.0.2454.93 Safari/537.36'
-                }
-                try:
-                    response = requests.request(feed_parsing_details["method"], feed_parsing_details["remote"],
-                                                proxies=cif.proxies, stream=True, headers=headers, verify=False
-                                                )
-                except:
-                    self.logging.exception("Could not fetch remote url '{0}'".format(feed_parsing_details["remote"]))
-                    continue
+                return
 
-                if response.status_code > 300:
-                    self.logging.error("Failed fetching remote feed '{0}': {1} {2}".format(
-                        feed_parsing_details["remote"], response.status_code, response.reason)
-                    )
-                    return
+            temp = tempfile.TemporaryFile(mode="w+b")
 
-                temp = tempfile.TemporaryFile(mode="w+b")
+            for chunk in response.iter_content(1024 * 1024):
+                temp.write(chunk)
 
-                for chunk in response.iter_content(1024 * 1024):
-                    temp.write(chunk)
+            response.close()
 
-                response.close()
+            self.logging.debug("Wrote {0} bytes to binary file.".format(temp.tell()))
 
-                self.logging.debug("Wrote {0} bytes to binary file.".format(temp.tell()))
+            temp.seek(0)
 
-                temp.seek(0)
+        zip_file = None
+        if zipfile.is_zipfile(temp):
+            zip_file = zipfile.ZipFile(temp)
+            temp_bin = zip_file.open(zip_file.namelist()[0])
+        else:
+            temp_bin = temp
+            temp_bin.seek(0)
 
-            zip_file = None
-            if zipfile.is_zipfile(temp):
-                zip_file = zipfile.ZipFile(temp)
-                temp_bin = zip_file.open(zip_file.namelist()[0])
+        if zip_file is None:
+            test_header = temp.read(2)
+            temp.seek(0)
+            if test_header == b'\x1f\x8b':
+                temp_bin = gzip.open(temp)
             else:
                 temp_bin = temp
-                temp_bin.seek(0)
 
-            if zip_file is None:
-                test_header = temp.read(2)
-                temp.seek(0)
-                if test_header == b'\x1f\x8b':
-                    temp_bin = gzip.open(temp)
-                else:
-                    temp_bin = temp
+        file_to_parse = tempfile.TemporaryFile(mode="w+", encoding="ISO8859-1")
+        while True:
+            buf = temp_bin.read(1024 * 1024)
+            if len(buf) == 0:
+                break
+            file_to_parse.write(buf.decode("ISO8859-1"))
+        self.logging.debug("Wrote {0} bytes to text file.".format(file_to_parse.tell()))
+        file_to_parse.seek(0)
 
-            file_to_parse = tempfile.TemporaryFile(mode="w+", encoding="ISO8859-1")
-            while True:
-                buf = temp_bin.read(1024 * 1024)
-                if len(buf) == 0:
-                    break
-                file_to_parse.write(buf.decode("ISO8859-1"))
-            self.logging.debug("Wrote {0} bytes to text file.".format(file_to_parse.tell()))
-            file_to_parse.seek(0)
+        if zip_file is not None:
+            zip_file.close()
+        temp_bin.close()
 
-            if zip_file is not None:
-                zip_file.close()
-            temp_bin.close()
+        self.logging.debug("Creating Parser for feed {0}".format(feed_parsing_details['remote']))
+        self.parser = Parser(parsing_details=feed_parsing_details, basemeta=feed_meta, file=file_to_parse)
 
-            self.logging.debug("Creating Parser for feed {0}".format(feed_parsing_details['remote']))
-            self.parser = Parser(parsing_details=feed_parsing_details, basemeta=feed_meta, file=file_to_parse)
+        while self.parser.parsing:
+            observables = self.parser.parsefile(2000)
+            if len(observables) > 0:
+                self.logging.debug("Feed '{0}' is sending {1} new objects to be processed".format(
+                    feed_parsing_details['remote'], len(observables))
+                )
+                for observable in observables:
+                    tasks.put(observable)
 
-            while self.parser.parsing:
-                observables = self.parser.parsefile(2000)
-                if len(observables) > 0:
-                    self.logging.debug("Feed '{0}' is sending {1} new objects to be processed".format(
-                        feed_parsing_details['remote'], len(observables))
-                    )
-                    for observable in observables:
-                        tasks.put(observable)
-
-            file_to_parse.close()
-            self.logging.debug("Finished Parsing feed {0}".format(feed_parsing_details['remote']))
+        file_to_parse.close()
+        self.logging.debug("Finished Parsing feed {0}".format(feed_parsing_details['remote']))

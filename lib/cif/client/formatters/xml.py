40f02a28fcd881b3a73e2e476b3b1a14988c2c39
James DeVincentis <james@hexhost.net>
2016-02-03 16:43:06 -0600

    Fix bugs

--- lib/cif/client/formatters/xml.py
+++ lib/cif/client/formatters/xml.py
@@ -3,17 +3,17 @@ import xml.etree.cElementTree
 __author__ = "Aaron Eppert <aaron.eppert@packetsled.com>"
 
 
-def process(self, options, results, output_handle):
+def process(options, results, output_handle):
     headers = options['select'].split(',')
 
     if options['write'] is not None:
-        self.output_handle.close()
-        self.output_handle = open(options['write'], 'wb')
+        output_handle.close()
+        output_handle = open(options['write'], 'wb')
     else:
         raise RuntimeError("XML output can only be written to a file.")
 
     observables = xml.etree.cElementTree.Element("observables")
-    for result in self.results:
+    for result in results:
         observable = xml.etree.cElementTree.SubElement(observables, "observable")
         for header in headers:
             if isinstance(result[header], list):
@@ -26,4 +26,4 @@ def process(self, options, results, output_handle):
                 xml.etree.cElementTree.SubElement(observable, header).text = result[header]
 
     tree = xml.etree.cElementTree.ElementTree(observables)
-    tree.write(self.output_handle)
+    tree.write(output_handle)
